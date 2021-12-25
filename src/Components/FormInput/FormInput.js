ae405209ea653a1ace6f0c01d840a524d772eb90
mudaser sayeedi <mudasersayeed@gmail.com>
2021-12-25 22:07:46 +0430

    some signIn and signOut route bugs removed

--- src/Components/FormInput/FormInput.js
+++ src/Components/FormInput/FormInput.js
@@ -1,9 +1,16 @@
 import './FormInput.css';
+import { useState } from 'react';
 
-const FormInput = ({type,label,placeholder}) => {
+const FormInput = ({ type, label, placeholder }) => {
+    const [ value, setValue ] = useState('');
+
+    const inputValue = (e) => {
+        setValue(e.target.value);
+    }
+
     return (
         <div className='input'>
-            <input className='input-type' type={type} placeholder={placeholder} formNoValidate/>
+            <input className='input-type' type={type} placeholder={placeholder} value={value} onChange={inputValue} formNoValidate/>
             <label className='input-label'>{label}</label>
         </div>
     );
